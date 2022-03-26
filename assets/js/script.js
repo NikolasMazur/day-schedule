3ba4c0f4cb3fa151036e52950cf82ffd1e08d426
kolemazur@gmail.com <kolemazur@gmail.com>
2022-03-26 15:14:39 -0600

    Add currentHour variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,20 @@
+var currentHour = moment().hour()
+
 $("#currentDay").text(moment().format("MMMM Do YYYY A"));
 
+// time blocks COLOR CODED to indicate past, present, or future
+$("textarea").each(function () {
+    var $this = $(this);
+    var id = parseInt($this.attr("id"));
+    
+    if (id < currentHour) {
+    $(this).addClass("past");
+    }
+    if (id > currentHour) {
+    $(this).addClass("future");
+    }
+    if (id === currentHour) {
+    $(this).addClass("present");
+    }
+    });
+    
